dac913624884d2851e6c12251db2a0e66815e7e7
Nathan Gonzalez <ncgonz@gmail.com>
2020-03-02 21:19:53 -0600

    Add project files.

---
+++ AccessibilityExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Animals.Reptiles;
+namespace AccessibilityExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var alligator = new Alligator();
+            //alligator.Grunt();
+
+        }
+    }
+}
---
+++ Animals/Reptiles/Alligator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Reptiles
+{
+    public class Alligator
+    {
+
+        //bool get_IsAquatic()
+        //{
+        //    return true;
+        //}
+
+        //private bool _isAquatic;
+
+        //public bool IsAquatic
+        //{
+        //    get { return _isAquatic; }
+        //    set { _isAquatic = value; }
+        //}
+
+        public bool IsAquatic { get; set; }
+        private int _stepGoal;
+        private int _stepCount;
+
+        public Alligator(int stepGoal)
+        {
+            IsAquatic = true;
+            _stepGoal = stepGoal;
+        }
+
+        internal void Grunt()
+        {
+            if (IsAquatic)
+            {
+                Console.WriteLine("Bubble Bubble");
+            }
+            else
+            {
+                Console.WriteLine("GRRRRRR!");
+            }
+        }
+
+        public void Run(int steps)
+        {
+            _stepCount += steps;
+
+            Console.WriteLine($"You ran {steps} steps.");
+
+            if (_stepCount >= _stepGoal)
+            {
+                Console.WriteLine("Congratulations, you achieved your goal!!!!!!!!!!!!!!!");
+            }
+        }
+    }
+}
